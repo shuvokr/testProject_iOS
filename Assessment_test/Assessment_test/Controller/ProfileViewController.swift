d89fd352be18875b4dc4d60904020362d19d9895
Hex_aad <shuvosksk@gmail.com>
2021-03-15 12:58:53 +0600

    - Configed project structure.

---
+++ Assessment_test/Assessment_test/Controller/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Assessment_test
+//
+//  Created by Shuvo on 15/3/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
